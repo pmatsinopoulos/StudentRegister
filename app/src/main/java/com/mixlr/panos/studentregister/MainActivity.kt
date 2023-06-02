176e2f13d64c1c2f98c93855fed91a9a18e26804
Panos Matsinopoulos <panos@mixlr.com>
2023-06-02 18:39:23 +0300

    Display the list of students from the database

--- app/src/main/java/com/mixlr/panos/studentregister/MainActivity.kt
+++ app/src/main/java/com/mixlr/panos/studentregister/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.mixlr.panos.studentregister.db.Student
 import com.mixlr.panos.studentregister.db.StudentDatabase
 
@@ -15,6 +17,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var clearButton: Button
 
     private lateinit var viewModel: StudentViewModel
+    private lateinit var studentRecyclerView: RecyclerView
+    private lateinit var adapter: StudentRecycleViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +28,7 @@ class MainActivity : AppCompatActivity() {
         emailEditText = findViewById(R.id.etEmail)
         saveButton = findViewById(R.id.btnSave)
         clearButton = findViewById(R.id.btnClear)
+        studentRecyclerView = findViewById(R.id.rvStudent)
 
         val dao = StudentDatabase.getInstance(application).studentDao()
         val factory = StudentViewModelFactory(dao)
@@ -37,6 +42,8 @@ class MainActivity : AppCompatActivity() {
         clearButton.setOnClickListener {
             clearInput()
         }
+
+        initRecyclerView()
     }
 
     private fun saveStudentData() {
@@ -52,5 +59,20 @@ class MainActivity : AppCompatActivity() {
     private fun clearInput() {
         nameEditText.setText("")
         emailEditText.setText("")
+    }
+
+    private fun initRecyclerView() {
+        studentRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = StudentRecycleViewAdapter()
+        studentRecyclerView.adapter = adapter
+
+        displayStudentsList()
+    }
+
+    private fun displayStudentsList() {
+        viewModel.students.observe(this) {
+            adapter.setList(it)
+            adapter.notifyDataSetChanged()
+        }
     }
 }
