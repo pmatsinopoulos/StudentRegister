e672a73c149fb9c51932ec1f0ce0229cd6a841d5
Panos Matsinopoulos <panos@mixlr.com>
2023-06-02 18:15:34 +0300

    Main Activity now invokes the ViewModel to add records to the database

--- app/src/main/java/com/mixlr/panos/studentregister/MainActivity.kt
+++ app/src/main/java/com/mixlr/panos/studentregister/MainActivity.kt
@@ -2,10 +2,55 @@ package com.mixlr.panos.studentregister
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.lifecycle.ViewModelProvider
+import com.mixlr.panos.studentregister.db.Student
+import com.mixlr.panos.studentregister.db.StudentDatabase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var nameEditText: EditText
+    private lateinit var emailEditText: EditText
+    private lateinit var saveButton: Button
+    private lateinit var clearButton: Button
+
+    private lateinit var viewModel: StudentViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nameEditText = findViewById(R.id.etName)
+        emailEditText = findViewById(R.id.etEmail)
+        saveButton = findViewById(R.id.btnSave)
+        clearButton = findViewById(R.id.btnClear)
+
+        val dao = StudentDatabase.getInstance(application).studentDao()
+        val factory = StudentViewModelFactory(dao)
+        viewModel = ViewModelProvider(this, factory).get(StudentViewModel::class.java)
+
+        saveButton.setOnClickListener {
+            saveStudentData()
+            clearInput()
+        }
+
+        clearButton.setOnClickListener {
+            clearInput()
+        }
+    }
+
+    private fun saveStudentData() {
+        viewModel.insertStudent(
+            Student(
+                0,
+                nameEditText.text.toString(),
+                emailEditText.text.toString()
+            )
+        )
+    }
+
+    private fun clearInput() {
+        nameEditText.setText("")
+        emailEditText.setText("")
     }
 }
