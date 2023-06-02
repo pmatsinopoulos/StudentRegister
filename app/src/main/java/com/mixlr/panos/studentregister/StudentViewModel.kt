aff1cf949067e42db9868383af3c588b9a6016e5
Panos Matsinopoulos <panos@mixlr.com>
2023-06-02 17:58:13 +0300

    Added StudentViewModel
    It takes as parameter in the constructor a StudeDao and returns
    a ViewModel.

---
+++ app/src/main/java/com/mixlr/panos/studentregister/StudentViewModel.kt
@@ -0,0 +1,23 @@
+package com.mixlr.panos.studentregister
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mixlr.panos.studentregister.db.Student
+import com.mixlr.panos.studentregister.db.StudentDao
+import kotlinx.coroutines.launch
+
+class StudentViewModel(private val dao: StudentDao) : ViewModel() {
+    val students = dao.getAllStudents()
+
+    fun insertStudent(student: Student) = viewModelScope.launch {
+        dao.insertStudent(student)
+    }
+
+    fun updateStudent(student: Student) = viewModelScope.launch {
+        dao.updateStudent(student)
+    }
+
+    fun deleteStudent(student: Student) = viewModelScope.launch {
+        dao.deleteStudent(student)
+    }
+}
